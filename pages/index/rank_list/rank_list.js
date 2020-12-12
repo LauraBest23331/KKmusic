829ed7eb0bd577cea861930a4d40ac91d883530c
ZhangYang <18111002318@163.com>
2020-12-12 16:46:11 +0800

    音乐功能实现

--- pages/index/rank_list/rank_list.js
+++ pages/index/rank_list/rank_list.js
@@ -5,14 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    songs: [],
+    playUrl: "/assets/images/stop.png",
+    stopUrl: "/assets/images/play.png",
+    bindUrl: []
   },
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
+ 
   onLoad: function (options) {
+    let that = this
+    wx.request({
+      url: 'https://musicapi.leanapp.cn/search?keywords=热歌', //仅为示例，并非真实的接口地址
+      data: {
 
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        // console.log(res.data.result.songs)
+        
+        that.setData({
+          songs : res.data.result.songs
+        })
+        let temp_bind = []
+        for (const key in that.data.songs) {
+          temp_bind[key] = true
+        }
+        that.setData({
+          bindUrl : temp_bind
+        })
+        console.log(that.data.bindUrl)
+        console.log(that.data.songs)
+      }
+    })
   },
 
   /**
@@ -28,39 +57,28 @@ Page({
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  play: function name(event) {
+    const now_id = event.target.dataset.cid
+    let temp = this.data.bindUrl
+    let isPlay;
+    for (const key in temp) {
+      if (key == now_id) {
+        temp[key] = !temp[key]
+        isPlay = temp[key];
+      }
+      else {
+        temp[key] = true
+      }
+    }
+    this.setData({
+      bindUrl:temp
+    })
+    
+    wx.playBackgroundAudio({
+      dataUrl: 'https://music.163.com/song/media/outer/url?id='+ this.data.songs[now_id].id +'.mp3',
+      title: '',
+      coverImgUrl: ''
+    })
+    console.log(this.data.bindUrl)
   }
 })
